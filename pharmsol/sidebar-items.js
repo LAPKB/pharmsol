5603bae925d9f986e02b8278fe30afea26e26fba
mhovd <mhovd@users.noreply.github.com>
2024-05-13 14:02:24 +0000

    deploy: 15728481b84f7b8143082f24964816b9bcd650a3

---
+++ pharmsol/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"macro":["fa","fetch_cov","fetch_params","lag"],"mod":["prelude"]};
---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"pharmsol":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"pharmsol/prelude/data/struct.Data.html\" title=\"struct pharmsol::prelude::data::Data\">Data</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"pharmsol/prelude/data/struct.Subject.html\" title=\"struct pharmsol::prelude::data::Subject\">Subject</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
