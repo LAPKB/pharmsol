6ff649a7c379fbfd0460548d087f110b2a298e39
Siel <Siel@users.noreply.github.com>
2024-07-28 11:14:12 +0000

    deploy: 6626211034947e08918fccd93d7bcc47b846d6e3

--- pharmsol/prelude/simulator/sidebar-items.js
+++ pharmsol/prelude/simulator/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["PopulationPredictions","Prediction","SubjectPredictions"]};
+window.SIDEBAR_ITEMS = {"fn":["pf_psi"],"struct":["PopulationPredictions","Prediction","SubjectPredictions"]};
