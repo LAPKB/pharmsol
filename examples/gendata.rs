2760c6bd286bdb9c536735e7d05e544ef95bc689
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-08-19 15:10:05 +0100

    two comp

--- examples/gendata.rs
+++ examples/gendata.rs
@@ -15,27 +15,28 @@ fn main() {
     let sde = simulator::Equation::new_sde(
         |x, p, _t, dx, _rateiv, _cov| {
             // automatically defined
-            fetch_params!(p, ke0, _ske, _v);
+            fetch_params!(p, ka, ke0, _ske, _v);
             let ke = x[1];
-            dx[1] = -ke + ke0;
+            dx[2] = -ke + ke0;
             // user defined
-            dx[0] = -ke * x[0];
+            dx[0] = -ka * x[0];
+            dx[1] = ka * x[0] - ke * x[1];
         },
         |p, d| {
-            fetch_params!(p, _ke0, ske, _v);
+            fetch_params!(p, _ka, _ke0, ske, _v);
             d[1] = ske;
         },
         |_p| lag! {},
         |_p| fa! {},
         |p, _t, _cov, x| {
-            fetch_params!(p, ke0, _ske, _v);
+            fetch_params!(p, _ka, ke0, _ske, _v);
             x[1] = ke0
         },
         |x, p, _t, _cov, y| {
-            fetch_params!(p, _ke0, _ske, v);
-            y[0] = x[0] / v;
+            fetch_params!(p, _ka, _ke0, _ske, v);
+            y[0] = x[1] / v;
         },
-        (2, 1),
+        (3, 1),
     );
 
     // let ke_dist = rand_distr::Normal::new(0.7, 0.15).unwrap();
@@ -46,11 +47,12 @@ fn main() {
     for _ in 0..10 {
         // let ke = ke_dist.sample(&mut rand::thread_rng());
         let ke = 0.7;
+        let ka = 0.3;
         let ske = ske_dist.sample(&mut rand::thread_rng());
         // let v = v_dist.sample(&mut rand::thread_rng());
         let v = 50.0;
-        support_points.push(vec![ke, ske, v]);
-        println!("{ke}, {ske}, {v}");
+        support_points.push(vec![ka, ke, ske, v]);
+        println!("{ka}, {ke}, {ske}, {v}");
     }
 
     let mut data = vec![];
--- src/simulator/sde/em.rs
+++ src/simulator/sde/em.rs
@@ -47,8 +47,8 @@ impl SDE {
         (self.diffusion)(&self.params.clone(), &mut diffusion_term);
 
         // Create a seeded RNG
-        // let mut rng = StdRng::seed_from_u64(0);
-        let mut rng = thread_rng();
+        let mut rng = StdRng::seed_from_u64(0);
+        // let mut rng = thread_rng();
 
         for i in 0..n {
             let normal_dist = Normal::new(0.0, 1.0).unwrap();
