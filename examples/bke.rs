acc932a764ce97e36b30a36922b808fa7f5900f4
Julian D. Otalvaro <juliandavid347@gmail.com>
2024-09-30 17:34:30 +0100

    support concatenated operations

--- examples/bke.rs
+++ examples/bke.rs
@@ -45,7 +45,7 @@ fn main() {
         vec![dmatrix![-1.0], dmatrix![0.0]],
         vec![],
         vec![],
-        vec![OutEq::new(0, Op::Div(X(0), P(1)))],
+        vec![OutEq::new(0, Div(X(0), P(1)))],
         (1, 1),
     );
     let em = ErrorModel::new((0.0, 0.05, 0.0, 0.0), 0.0, &ErrorType::Add);
