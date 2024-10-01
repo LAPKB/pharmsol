4f333aedbf9bddfaca805e7cef491e7d62cc75ec
github-action-benchmark <github@users.noreply.github.com>
2024-10-01 12:21:07 +0000

    add Criterion.rs Benchmark (cargo) benchmark result for 472b04fc148e184b7b55444f57bc5dae7411fa69

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1727785267855,
+  "repoUrl": "https://github.com/LAPKB/Pharmsol",
+  "entries": {
+    "Criterion.rs Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "markushh@uio.no",
+            "name": "Markus Hovd",
+            "username": "mhovd"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "472b04fc148e184b7b55444f57bc5dae7411fa69",
+          "message": "Summary to benchmark job",
+          "timestamp": "2024-10-01T14:19:23+02:00",
+          "tree_id": "645b888409ab117803d9cdfee59132c6fea5317c",
+          "url": "https://github.com/LAPKB/Pharmsol/commit/472b04fc148e184b7b55444f57bc5dae7411fa69"
+        },
+        "date": 1727785266719,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "readme 20",
+            "value": 1400802,
+            "range": "± 25356",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
