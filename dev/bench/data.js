e49f99c0935d59787a8dc632119f7284666da86d
github-action-benchmark <github@users.noreply.github.com>
2024-10-01 12:39:40 +0000

    add Criterion.rs Benchmark (cargo) benchmark result for 0b202878c30b28ff5d7b3ea4188de7a0f0a2de23

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1727786380639,
+  "repoUrl": "https://github.com/LAPKB/pharmsol",
+  "entries": {
+    "Criterion.rs Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "66058642+mhovd@users.noreply.github.com",
+            "name": "Markus",
+            "username": "mhovd"
+          },
+          "committer": {
+            "email": "66058642+mhovd@users.noreply.github.com",
+            "name": "Markus",
+            "username": "mhovd"
+          },
+          "distinct": true,
+          "id": "0b202878c30b28ff5d7b3ea4188de7a0f0a2de23",
+          "message": "Update README.md",
+          "timestamp": "2024-10-01T14:37:57+02:00",
+          "tree_id": "1288cf9dc0b0b02c3f430401ab209aaa64188894",
+          "url": "https://github.com/LAPKB/pharmsol/commit/0b202878c30b28ff5d7b3ea4188de7a0f0a2de23"
+        },
+        "date": 1727786380204,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "readme 20",
+            "value": 1385290,
+            "range": "± 15304",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
