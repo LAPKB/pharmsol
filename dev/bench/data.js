281525197631ebe7c1ac9e38db7ab39af76937bf
github-action-benchmark <github@users.noreply.github.com>
2024-10-01 13:35:42 +0000

    add Benchmark (cargo) benchmark result for 2795ff372c00b9b4dbbcffd15d6aa792dcc92a95

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727789589071,
+  "lastUpdate": 1727789742875,
   "repoUrl": "https://github.com/LAPKB/pharmsol",
   "entries": {
     "Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "readme 20",
             "value": 1394193,
             "range": "± 25032",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markushh@uio.no",
+            "name": "Markus Hovd",
+            "username": "mhovd"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2795ff372c00b9b4dbbcffd15d6aa792dcc92a95",
+          "message": "Update benchmark.yml",
+          "timestamp": "2024-10-01T15:31:32+02:00",
+          "tree_id": "4cc3e7684d6a09b7562ce5b19ffc1a79849510b1",
+          "url": "https://github.com/LAPKB/pharmsol/commit/2795ff372c00b9b4dbbcffd15d6aa792dcc92a95"
+        },
+        "date": 1727789741746,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "readme 20",
+            "value": 1371918,
+            "range": "± 12124",
             "unit": "ns/iter"
           }
         ]
