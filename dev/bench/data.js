9fd83a2d6d1ed5a6c10cbc8ca9d99414990afd30
github-action-benchmark <github@users.noreply.github.com>
2024-10-01 12:46:04 +0000

    add Criterion.rs Benchmark (cargo) benchmark result for 607abbed6bcbad8013eeb2e2f528dba8f0149d73

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1727786764334,
+  "repoUrl": "https://github.com/LAPKB/pharmsol",
+  "entries": {
+    "Criterion.rs Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "juliandavid347@gmail.com",
+            "name": "Julian Otalvaro",
+            "username": "Siel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "607abbed6bcbad8013eeb2e2f528dba8f0149d73",
+          "message": "Merge pull request #9 from LAPKB/net\n\nNet",
+          "timestamp": "2024-10-01T13:44:21+01:00",
+          "tree_id": "a7e6f5106268458162f7a2d36a72ecd179d5d3d4",
+          "url": "https://github.com/LAPKB/pharmsol/commit/607abbed6bcbad8013eeb2e2f528dba8f0149d73"
+        },
+        "date": 1727786763795,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "readme 20",
+            "value": 1428125,
+            "range": "± 28923",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
