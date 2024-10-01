bd3f444b62328f849ec1c86d28ea2ef456eb2eae
github-action-benchmark <github@users.noreply.github.com>
2024-10-01 12:38:21 +0000

    add Criterion.rs Benchmark (cargo) benchmark result for 71b3a228cef9fd6f0e374bf577ec79e2a7e5140e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727785267855,
-  "repoUrl": "https://github.com/LAPKB/Pharmsol",
+  "lastUpdate": 1727786301463,
+  "repoUrl": "https://github.com/LAPKB/pharmsol",
   "entries": {
     "Criterion.rs Benchmark": [
       {
@@ -29,6 +29,32 @@ window.BENCHMARK_DATA = {
             "name": "readme 20",
             "value": 1400802,
             "range": "± 25356",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "LAPKB",
+            "username": "LAPKB"
+          },
+          "committer": {
+            "name": "LAPKB",
+            "username": "LAPKB"
+          },
+          "id": "71b3a228cef9fd6f0e374bf577ec79e2a7e5140e",
+          "message": "Net",
+          "timestamp": "2024-10-01T12:34:43Z",
+          "url": "https://github.com/LAPKB/pharmsol/pull/9/commits/71b3a228cef9fd6f0e374bf577ec79e2a7e5140e"
+        },
+        "date": 1727786300425,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "readme 20",
+            "value": 1420172,
+            "range": "± 29546",
             "unit": "ns/iter"
           }
         ]
