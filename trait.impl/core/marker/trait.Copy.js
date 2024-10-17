8df847e490cdcb56b6a0b4fe98bc979a0c108003
mhovd <mhovd@users.noreply.github.com>
2024-10-17 17:14:37 +0000

    deploy: bc25ccaa39473dd01c88d5be216148853e3292b5

--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"pharmsol":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"pharmsol/prelude/simulator/struct.Prediction.html\" title=\"struct pharmsol::prelude::simulator::Prediction\">Prediction</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["pharmsol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"pharmsol/prelude/simulator/struct.Prediction.html\" title=\"struct pharmsol::prelude::simulator::Prediction\">Prediction</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[312]}
