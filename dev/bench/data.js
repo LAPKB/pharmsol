863302f17cf0dda8cb64588d9020f11828c8a3d9
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 17:15:52 +0000

    add Benchmark (cargo) benchmark result for bc25ccaa39473dd01c88d5be216148853e3292b5

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1729185352647,
+  "repoUrl": "https://github.com/LAPKB/pharmsol",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "66058642+mhovd@users.noreply.github.com",
+            "name": "Markus",
+            "username": "mhovd"
+          },
+          "committer": {
+            "email": "66058642+mhovd@users.noreply.github.com",
+            "name": "Markus",
+            "username": "mhovd"
+          },
+          "distinct": true,
+          "id": "bc25ccaa39473dd01c88d5be216148853e3292b5",
+          "message": "Implement from_occasion for Subject\n\nVersion bump for release",
+          "timestamp": "2024-10-17T19:13:46+02:00",
+          "tree_id": "8caf94c2aa9b7a030efe289b1c1f3c5b0833d4f9",
+          "url": "https://github.com/LAPKB/pharmsol/commit/bc25ccaa39473dd01c88d5be216148853e3292b5"
+        },
+        "date": 1729185352097,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "SubjectBuilder simple",
+            "value": 156,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "SubjectBuilder covariates",
+            "value": 735,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "readme 20",
+            "value": 1377096,
+            "range": "± 42310",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
