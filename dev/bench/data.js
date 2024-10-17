82a18eacca1cf4ddd96b4b4c41e29d27daca2a60
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 12:37:03 +0000

    add Benchmark (cargo) benchmark result for 1194ed8d71cd5580767924bc29bbf798b2d93fc7

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1729168623491,
+  "repoUrl": "https://github.com/LAPKB/pharmsol",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "juliandavid347@gmail.com",
+            "name": "Julian Otalvaro",
+            "username": "Siel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1194ed8d71cd5580767924bc29bbf798b2d93fc7",
+          "message": "Merge pull request #10 from LAPKB/net\n\nsupport for addl",
+          "timestamp": "2024-10-17T13:35:00+01:00",
+          "tree_id": "1a0489bd957d4deeebe79d0e75e1b94f6f409cd5",
+          "url": "https://github.com/LAPKB/pharmsol/commit/1194ed8d71cd5580767924bc29bbf798b2d93fc7"
+        },
+        "date": 1729168621985,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "SubjectBuilder simple",
+            "value": 162,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "SubjectBuilder covariates",
+            "value": 707,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "readme 20",
+            "value": 1377808,
+            "range": "± 32919",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
