e90acbfb16b890f178c0c244af0cc870945fdb97
github-action-benchmark <github@users.noreply.github.com>
2025-02-05 13:32:55 +0000

    add Benchmark (cargo) benchmark result for 60409c12b8a0c6d62688b374b1447e06b5cf5d8b

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1738762375033,
+  "repoUrl": "https://github.com/LAPKB/pharmsol",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "juliandavid347@gmail.com",
+            "name": "Julian D. Otalvaro",
+            "username": "Siel"
+          },
+          "committer": {
+            "email": "juliandavid347@gmail.com",
+            "name": "Julian D. Otalvaro",
+            "username": "Siel"
+          },
+          "distinct": false,
+          "id": "60409c12b8a0c6d62688b374b1447e06b5cf5d8b",
+          "message": "Deserialize for Data structs",
+          "timestamp": "2025-01-27T14:18:06Z",
+          "tree_id": "d9d5685fe0d24140771b1bab0d0af5dc2ef5d093",
+          "url": "https://github.com/LAPKB/pharmsol/commit/60409c12b8a0c6d62688b374b1447e06b5cf5d8b"
+        },
+        "date": 1738762374481,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "SubjectBuilder simple",
+            "value": 158,
+            "range": "± 10",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "SubjectBuilder covariates",
+            "value": 664,
+            "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
