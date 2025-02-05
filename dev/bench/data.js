c773451eab5e7fc5446ee5e1aacbb839dcfa0930
github-action-benchmark <github@users.noreply.github.com>
2025-02-05 13:33:07 +0000

    add Benchmark (cargo) benchmark result for e43eaf849beea50708dee6a95797fdf2c045fd1a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738762375033,
+  "lastUpdate": 1738762387425,
   "repoUrl": "https://github.com/LAPKB/pharmsol",
   "entries": {
     "Benchmark": [
@@ -35,6 +35,42 @@ window.BENCHMARK_DATA = {
             "name": "SubjectBuilder covariates",
             "value": 664,
             "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "juliandavid347@gmail.com",
+            "name": "Julian D. Otalvaro",
+            "username": "Siel"
+          },
+          "committer": {
+            "email": "juliandavid347@gmail.com",
+            "name": "Julian D. Otalvaro",
+            "username": "Siel"
+          },
+          "distinct": true,
+          "id": "e43eaf849beea50708dee6a95797fdf2c045fd1a",
+          "message": "V0.7.2",
+          "timestamp": "2025-02-05T13:30:52Z",
+          "tree_id": "1b6d9112a317ab64be8b0154ccda72ba0bfb5257",
+          "url": "https://github.com/LAPKB/pharmsol/commit/e43eaf849beea50708dee6a95797fdf2c045fd1a"
+        },
+        "date": 1738762386926,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "SubjectBuilder simple",
+            "value": 163,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "SubjectBuilder covariates",
+            "value": 696,
+            "range": "± 2",
             "unit": "ns/iter"
           }
         ]
