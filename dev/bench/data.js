3da6cc72a67a455b341e97fc9f6e888014a5e6d8
github-action-benchmark <github@users.noreply.github.com>
2025-02-06 06:51:09 +0000

    add Benchmark (cargo) benchmark result for c6a8209666cdf980e7a4a372fe479d5ab395cf1f

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1738824669818,
+  "repoUrl": "https://github.com/LAPKB/pharmsol",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "markushh@uio.no",
+            "name": "Markus Hovd",
+            "username": "mhovd"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6a8209666cdf980e7a4a372fe479d5ab395cf1f",
+          "message": "Merge pull request #16 from LAPKB/dependabot/cargo/cached-0.54.0\n\nUpdate cached requirement from 0.53.1 to 0.54.0",
+          "timestamp": "2025-02-06T07:48:54+01:00",
+          "tree_id": "5dbc2eea0e3e2984f462b215b0ca17eafb60589e",
+          "url": "https://github.com/LAPKB/pharmsol/commit/c6a8209666cdf980e7a4a372fe479d5ab395cf1f"
+        },
+        "date": 1738824669158,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "SubjectBuilder simple",
+            "value": 157,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "SubjectBuilder covariates",
+            "value": 684,
+            "range": "± 6",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
