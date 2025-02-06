c4e7575015f86983a53a63622ec00b79e772277b
github-action-benchmark <github@users.noreply.github.com>
2025-02-06 06:51:30 +0000

    add Benchmark (cargo) benchmark result for fb8e74e567b20e190aa62794ca25180dea75f98e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738824669818,
+  "lastUpdate": 1738824690853,
   "repoUrl": "https://github.com/LAPKB/pharmsol",
   "entries": {
     "Benchmark": [
@@ -35,6 +35,42 @@ window.BENCHMARK_DATA = {
             "name": "SubjectBuilder covariates",
             "value": 684,
             "range": "± 6",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "markushh@uio.no",
+            "name": "Markus Hovd",
+            "username": "mhovd"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fb8e74e567b20e190aa62794ca25180dea75f98e",
+          "message": "Merge pull request #15 from LAPKB/dependabot/cargo/thiserror-2.0.11\n\nUpdate thiserror requirement from 1.0.61 to 2.0.11",
+          "timestamp": "2025-02-06T07:49:12+01:00",
+          "tree_id": "18cab28767b01059d839a7c33032b27c6c328eed",
+          "url": "https://github.com/LAPKB/pharmsol/commit/fb8e74e567b20e190aa62794ca25180dea75f98e"
+        },
+        "date": 1738824689647,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "SubjectBuilder simple",
+            "value": 154,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "SubjectBuilder covariates",
+            "value": 668,
+            "range": "± 9",
             "unit": "ns/iter"
           }
         ]
