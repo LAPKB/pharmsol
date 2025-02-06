a1ed064ba751e9cf37290cbf6aade70bfed73c62
github-action-benchmark <github@users.noreply.github.com>
2025-02-06 07:48:36 +0000

    add Benchmark (cargo) benchmark result for 96d1e5cbda0b646ebe33b45c86a6b3243f0e652e

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1738828116618,
+  "repoUrl": "https://github.com/LAPKB/pharmsol",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "markushh@uio.no",
+            "name": "Markus Hovd",
+            "username": "mhovd"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "96d1e5cbda0b646ebe33b45c86a6b3243f0e652e",
+          "message": "Merge pull request #17 from LAPKB/dependabot/cargo/rand_distr-0.5.0\n\nUpdate rand_distr requirement from 0.4.3 to 0.5.0",
+          "timestamp": "2025-02-06T08:46:25+01:00",
+          "tree_id": "fe34374e27bb8ce1ec2705b4cb193b4657981ec3",
+          "url": "https://github.com/LAPKB/pharmsol/commit/96d1e5cbda0b646ebe33b45c86a6b3243f0e652e"
+        },
+        "date": 1738828115921,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "SubjectBuilder simple",
+            "value": 164,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "SubjectBuilder covariates",
+            "value": 713,
+            "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
