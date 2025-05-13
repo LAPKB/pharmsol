60884441645ef6e8c9ca0727d9341fb7481410c0
Markus <66058642+mhovd@users.noreply.github.com>
2025-05-13 10:47:33 +0200

    Handle CSV errors

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,6 +1,7 @@
 use thiserror::Error;
 
 use crate::data::error_model::ErrorModelError;
+use crate::data::parse_pmetrics::PmetricsError;
 use ndarray::ShapeError;
 
 #[derive(Error, Debug, Clone)]
@@ -9,4 +10,6 @@ pub enum PharmsolError {
     ErrorModelError(#[from] ErrorModelError),
     #[error("Shape error: {0}")]
     NdarrayShapeError(#[from] ShapeError),
+    #[error("Error parsing Pmetrics datafile: {0}")]
+    PmetricsError(#[from] PmetricsError),
 }
